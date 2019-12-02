aaa2cdbb3bd059e58f1ea78aa900cb1eb8183bde
xbaobaox <xbaobaox@sina.com>
2019-12-02 22:42:12 +0800

    12.2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,60 +1,147 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <div>{{ name }}</div>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+ <div class="subject">
+    <div class="search">
+    </div>
+    <ul class="flex">
+      <li v-for="(item,index) of menu" :key="index" class="li">
+        <div class="content">
+          <div class="icon" :style="item.color">
+            <img :src="item.icon" class="icon_image" />
+          </div>
+          <div class="title">{{item.title}}</div>
+        </div>
+      </li>
     </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
-</template>
 
+  </div>
+  
+</template>
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     msg: String,
     name: String
-  }
-}
+  },
+  data() {
+    return {
+      menu: [
+        {
+          id: 1,
+          icon: require("../assets/美食.png"),
+          title: "美食",
+          color: "background:rgb(253,157,33)"
+        },
+        {
+          id: 2,
+          icon: require("../assets/猫眼电影.png"),
+          title: "猫眼电影",
+          color: "background:rgb(255,103,103)"
+        },
+        {
+          id: 3,
+          icon: require("../assets/酒店.png"),
+          title: "酒店",
+          color: "background:rgb(138,144,250)"
+        },
+        {
+          id: 4,
+          icon: require("../assets/休闲娱乐.png"),
+          title: "休闲娱乐",
+          color: "background:rgb(255,207,82)"
+        },
+        {
+          id: 5,
+          icon: require("../assets/外卖.png"),
+          title: "外卖",
+          color: "background:rgb(253,157,33)"
+        },
+        {
+          id: 6,
+          icon: require("../assets/ktv.png"),
+          title: "KTV",
+          color: "background:rgb(255,207,82)"
+        },
+        {
+          id: 7,
+          icon: require("../assets/周边游.png"),
+          title: "周边游",
+          color: "background:rgb(77,198,238)"
+        },
+        {
+          id: 8,
+          icon: require("../assets/丽人.png"),
+          title: "丽人",
+          color: "background:rgb(255,128,194)"
+        },
+        {
+          id: 9,
+          icon: require("../assets/小吃快餐.png"),
+          title: "小吃快餐",
+          color: "background:rgb(255,156,62)"
+        },
+        {
+          id: 10,
+          icon: require("../assets/省略号.png"),
+          title: "全部分类",
+          color: "background:rgb(0,211,190)"
+        }
+      ]
+    };
+  },
+  methods: {}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
+* {
   padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
+  margin: 0;
 }
 a {
-  color: #42b983;
+  text-decoration: none;
+}
+li {
+  list-style: none;
+}
+body {
+  
+
+}
+.search{
+  height: 3rem;
+  background-color: rgb(255,205,67)
+}
+.li {
+  /* padding: 0.2rem 0; */
+  margin-bottom: 20px;
+  display: inline-block;
+  width: 20%;
+  text-align: center;
+}
+
+.icon {
+  width: 40px;
+  height: 40px;
+  margin: 0 auto;
+  border-radius: 50%;
+  background-color: #fd9d21;
+  position: relative;
+}
+.icon_image {
+  width: 25px;
+  height: 23px;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+}
+.title {
+  font-size: 12px;
+  padding-top: 7px;
+  color: #666;
 }
 </style>
